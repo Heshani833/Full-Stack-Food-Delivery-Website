0b235ae17a54e0ad9b58a73a81a482b9df5d1390
Heshani833 <heshanishehana123@gmail.com>
2025-08-19 23:11:24 +0530

    update navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,51 @@
 // Navbar.jsx
-import React,{useState} from 'react';
-import './Navbar.css';
-import logo from '../../assets/logo.png'; 
-import search_icon from '../../assets/search_icon.png';
-import basket_icon from '../../assets/basket_icon.png';
+import React, { useState } from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import search_icon from "../../assets/search_icon.png";
+import basket_icon from "../../assets/basket_icon.png";
 
 const Navbar = () => {
-const [menu, setMenu] = useState("home");
+  const [menu, setMenu] = useState("home");
 
   return (
     <div className="navbar">
       <img src={logo} alt="Logo" className="logo" />
 
       <ul className="navbar-menu">
-        <li>Home</li>
-        <li>Menu</li>
-        <li>Mobile App</li>
-        <li>Contact Us</li>
+        <li
+          className={menu === "home" ? "active" : ""}
+          onClick={() => setMenu("home")}
+        >
+          Home
+        </li>
+        <li
+          className={menu === "menu" ? "active" : ""}
+          onClick={() => setMenu("menu")}
+        >
+          Menu
+        </li>
+        <li
+          className={menu === "mobile-app" ? "active" : ""}
+          onClick={() => setMenu("mobile-app")}
+        >
+          Mobile App
+        </li>
+        <li
+          className={menu === "contact-us" ? "active" : ""}
+          onClick={() => setMenu("contact-us")}
+        >
+          Contact Us
+        </li>
       </ul>
 
       <div className="navbar-right">
         <img src={search_icon} alt="Search" className="search-icon" />
         <div className="navbar-search-icon">
-            <img src={basket_icon} alt="" className="basket-icon" />
-            <div className='dot'></div>
-            </div>
-            <button>Sign In</button>
+          <img src={basket_icon} alt="" className="basket-icon" />
+          <div className="dot"></div>
+        </div>
+        <button>Sign In</button>
       </div>
     </div>
   );
