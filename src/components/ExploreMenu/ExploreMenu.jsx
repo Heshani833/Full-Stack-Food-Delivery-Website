c033aed3f98bb6c3bef3115aad61e23a9d06c608
Heshani833 <heshanishehana123@gmail.com>
2025-08-20 20:50:10 +0530

    create ExploreMenu file

---
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './ExploreMenu.css';
+
+const ExploreMenu = () => {
+  return (
+    <div className="explore-menu">
+      <h2>Explore Our Menu</h2>
+      <p>Discover a variety of delicious dishes crafted by our chefs.</p>
+      <button>View Full Menu</button>
+    </div>
+  )
+}
+
+export default ExploreMenu
