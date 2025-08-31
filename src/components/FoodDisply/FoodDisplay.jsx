21547f9cedbdd17613bb360bacedc88c32d8e191
Heshani833 <heshanishehana123@gmail.com>
2025-08-31 17:28:31 +0530

    food displaying part 1

---
+++ src/components/FoodDisply/FoodDisplay.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./FoodDisplay.css"
+
+const FoodDisplay = () => {
+const { food_list } = useContext(StoreContext);
+
+  return (
+    <div>FoodDisplay</div>
+  )
+}
+
+export default FoodDisplay
