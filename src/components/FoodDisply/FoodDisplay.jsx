cb8c695f1c2dcf84e6118768a7e19f5a0d6bfe17
Heshani833 <heshanishehana123@gmail.com>
2025-09-05 22:49:24 +0530

    create food item file

--- src/components/FoodDisply/FoodDisplay.jsx
+++ src/components/FoodDisply/FoodDisplay.jsx
@@ -8,6 +8,11 @@ const FoodDisplay = ({ category }) => {
   return (
     <div className="food-display" id="food-display">
       <h2>Food Display</h2>
+      <div className="food-display-list">
+{food_list.map((item, index)=>{
+return
+})}
+      </div>
     </div>
   );
 };
