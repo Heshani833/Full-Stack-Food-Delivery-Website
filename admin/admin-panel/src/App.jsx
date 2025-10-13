6cecca1ec856a842d62512082fb2d4df752e0475
Heshani833 <heshanishehana123@gmail.com>
2025-10-13 15:06:11 +0530

    created admin navbar

--- admin/admin-panel/src/App.jsx
+++ admin/admin-panel/src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
@@ -8,6 +10,11 @@ const App = () => {
       <hr />
       <div className="app-content">
         <Sidebar />
+        <Routes>
+          <Route path="/add" element={<div>Add Items Page</div>} />
+          <Route path="/list" element={<div>List Items Page</div>} />
+          <Route path="/orders" element={<div>Orders Page</div>} />
+        </Routes>
       </div>
     </div>
   );
