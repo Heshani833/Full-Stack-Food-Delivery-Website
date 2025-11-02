dae6ec0d987bd9796ec3369d1ef62e2769a2b5b0
Heshani833 <heshanishehana123@gmail.com>
2025-11-02 23:14:50 +0530

    checked fetchLists function

--- admin/admin-panel/src/pages/List/List.jsx
+++ admin/admin-panel/src/pages/List/List.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import "./list.css"
+import React, { useEffect } from "react";
+import "./list.css";
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const List = () => {
-  return (
-    <div className="list">List</div>
-  )
-}
+  const url = "http://localhost:4000";
 
-export default List
+  const [lists, setLists] = useState([]);
+
+  const fetchLists = async () => {
+    const response = await axios.get(`${url}/api/food/list`);
+    console.log(response.data);
+    if (response.data.success) {
+      setLists(response.data.data);
+    } else {
+      toast.error(response.data.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchLists();
+  }, []);
+  return <div></div>;
+};
+
+export default List;
