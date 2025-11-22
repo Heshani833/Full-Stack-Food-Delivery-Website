313469e64942bea7f444523e469db1bc4bd625c7
Heshani833 <heshanishehana123@gmail.com>
2025-11-22 11:01:03 +0530

    Admin Food Remove Function

--- admin/admin-panel/src/pages/List/List.jsx
+++ admin/admin-panel/src/pages/List/List.jsx
@@ -19,10 +19,52 @@ const List = () => {
     }
   };
 
+  const removeFood = async (foodId) => {
+    try {
+      const response = await axios.post(`${url}/api/food/remove`, {
+        id: foodId,
+      });
+      await fetchLists();
+      if (response.data.success) {
+        toast.success(response.data.message);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      toast.error("Error removing food");
+    }
+  };
+
   useEffect(() => {
     fetchLists();
   }, []);
-  return <div></div>;
+  return (
+    <div className="list add flex-col">
+      <p>All Food LList</p>
+      <div className="list-table">
+        <div className="list-table-format tittle">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Actions</b>
+        </div>
+        {lists.map((item, index) => {
+          return (
+            <div key={item._id} className="list-table-format">
+              <img src={`${url}/images/` + item.image} alt="" />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>${item.price}</p>
+              <p onClick={() => removeFood(item._id)} className="cursor">
+                x
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default List;
