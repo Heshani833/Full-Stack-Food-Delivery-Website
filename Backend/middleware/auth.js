fecbdf912fa23cf9e86855d4222460faaafb17dd
Heshani833 <heshanishehana123@gmail.com>
2025-12-10 22:48:19 +0530

    Backend cartController _ add items

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -1,6 +1,17 @@
 import jwt from 'jsonwebtoken';
 
 const authMiddleware = (req, res, next) => {
+    const {token} = req.headers;
+    if (!token) {
+        return res.json({ success: false, message: 'No token provided' });
+    }
+    try {
+        const token_decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.body.userId = token_decoded.id;
+        next();
+    } catch (error) {
+        return res.json({ success: false, message: 'Invalid token' });
+    }
 }
 
 export default authMiddleware;
