7c97219de12b4ff03f14eafa9b73f1987fa2f0e3
Heshani833 <heshanishehana123@gmail.com>
2025-12-21 23:06:12 +0530

    styling myorder jsx

--- src/Pages/MyOrder/MyOrder.jsx
+++ src/Pages/MyOrder/MyOrder.jsx
@@ -26,21 +26,28 @@ const MyOrder = () => {
   }, [token]);
 
   return (
-    <div className="my-order">
+    <div className="my-orders">
       <h2>My Orders</h2>
       <div className="container">
         {data.map((order, index) => (
-          <div key={index} className="my-orders-order">
+          <div key={index} className="my-orders-orders">
             <img src={assets.parcel_icon} alt="order-image" />
             <p>
               {order.items.map((item, index) => {
                 if (index === order.items.length - 1) {
                   return item.name + "x" + item.quantity;
-                }else {
+                } else {
                   return item.name + "x" + item.quantity + ", ";
                 }
               })}
             </p>
+
+            <p>${order.amount}.00</p>
+            <p>Items: {order.items.length}</p>
+            <p>
+              <span>&#x25cf</span> <b>{order.status}</b>{" "}
+            </p>
+            <button>Track Order</button>
           </div>
         ))}
       </div>
