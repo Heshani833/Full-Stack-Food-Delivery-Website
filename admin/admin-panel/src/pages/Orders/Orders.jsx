a8dea3c186ecf890a44779ae71669d22f43b7cae
Heshani833 <heshanishehana123@gmail.com>
2025-12-22 13:33:20 +0530

    Admin orders.jsx

--- admin/admin-panel/src/pages/Orders/Orders.jsx
+++ admin/admin-panel/src/pages/Orders/Orders.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { use } from 'react'
 import "./orders.css"
+import axios from 'axios'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import toast from 'react-hot-toast'
 
-const Orders = () => {
+const Orders = ({url}) => {
+
+const [orders, setOrders] = useState([])
+
+const fetchOrders = async () => {
+  const response = await axios.get(url + "/api/orders/list");
+  if(response.data.success){
+    setOrders(response.data.data)
+    console.log(response.data.data)
+  }
+  else{
+    toast.error("Error fetching orders"); 
+  }
+   
+}
+
+useEffect(() => {
+  fetchOrders();
+}, [])
+
   return (
     <div className="orders">Orders</div>
   )
